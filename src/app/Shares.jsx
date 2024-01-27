9c7661dcf8731cecd69629510a9e56ec3c6cf863
Simon Picard <simon.picard94@gmail.com>
2024-01-27 01:09:33 -0500

    use npm

--- src/app/Shares.jsx
+++ src/app/Shares.jsx
@@ -5,7 +5,7 @@ export default function Shares({etfs}) {
     return (
         <div>
             {etfs.map(etf => (
-                <div className="flex justify-between text-xl">
+                <div className="flex justify-between text-xl" key={etf.etf}>
                     <span className="mr-3">{etf.etf}</span>
                     <span>{etf.shares.toLocaleString()} <FontAwesomeIcon icon={faBitcoinSign}/></span>
                 </div>
